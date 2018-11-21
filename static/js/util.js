d874f495b8e323277a8103020ea9375f478ca9b2
tomoya09 <pengjh@cpchain.io>
2018-11-21 21:00:48 +0800

    update timestamp convertor

--- static/js/util.js
+++ static/js/util.js
@@ -1,7 +1,7 @@
 function formatTS(mss) {
-	var hours = parseInt((mss % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = parseInt((mss % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = parseInt((mss % (1000 * 60)) / 1000);
+	var hours = parseInt((mss % (1 * 60 * 60 * 24)) / (1 * 60 * 60));
+	var minutes = parseInt((mss % (1 * 60 * 60)) / (1 * 60));
+	var seconds = parseInt((mss % (1 * 60)) / 1);
 	var res = ''
 	if (hours > 0) {
 		res = `${hours} hr${hours>1? 's': ''} ${minutes} min${minutes>1? 's': ''} ${seconds} sec${seconds>1? 's': ''}`
