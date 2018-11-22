d996b7136f94635a5b54772da808f31b2ca35e3f
wujj <wujj@cpchain.io>
2018-11-22 16:55:00 +0800

    deploy

--- static/js/default.js
+++ static/js/default.js
@@ -5,9 +5,9 @@ var ws;
 var socketurl;
 var lastprice;
 if (location.protocol === 'https:') {
-    socketurl = "wss://" + window.location.hostname + ":8000/explorer/wshandler/";
+    socketurl = "wss://" + window.location.hostname + "/explorer/wshandler/";
 } else {
-    socketurl = "ws://" + window.location.hostname + ":8000/explorer/wshandler/";
+    socketurl = "ws://" + window.location.hostname + "/explorer/wshandler/";
 }
 
 $(function () {
