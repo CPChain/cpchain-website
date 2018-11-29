8f2a4c86e321ecc51a44a41d6a02a79ae49df2c5
wujj <wujj@cpchain.io>
2018-11-29 11:34:14 +0800

    add rnodes committees

--- apps/explorer/views.py
+++ apps/explorer/views.py
@@ -329,11 +329,15 @@ def address(req, address):
 
 
 def rnode(req):
-    epoch = cf.cpc.getCurrentEpoch()
+    epoch = cf.cpc.getCurrentTerm()
     rnodes = cf.cpc.getRNodes()
     return render(req, 'explorer/rnode.html', {'epoch': epoch,
                                                'rnodes': rnodes})
 
 
 def committee(req):
-    return render(req, 'explorer/committee.html')
+    epoch = cf.cpc.getCurrentTerm()
+    round = cf.cpc.getCurrentView()
+    committees = cf.cpc.getCommittees()
+
+    return render(req, 'explorer/committee.html',locals())
