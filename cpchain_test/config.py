9ecc53d7201ad45b604e364e2d13db194ced9b06
wujj <wujj@cpchain.io>
2019-03-12 10:26:36 +0800

    0.135 initial

--- cpchain_test/config.py
+++ cpchain_test/config.py
@@ -4,7 +4,7 @@ import os
 parser = configparser.ConfigParser()
 
 if 'config.ini' in os.listdir(os.path.dirname(__file__)):
-    parser.read(os.path.join(os.path.dirname(__file__),'config.ini'))
+    parser.read(os.path.join(os.path.dirname(__file__), '.config.ini'))
 else:
     parser.read(os.path.join(os.path.dirname(__file__),'config.default.ini'))
 
