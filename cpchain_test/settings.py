10bf76dc04a7d21348ef6a1b1ce724f661ca95da
zhangkai <zhangk@cpchain.io>
2019-04-12 01:57:42 -0700

    settings

--- cpchain_test/settings.py
+++ cpchain_test/settings.py
@@ -220,7 +220,6 @@ PAGINATION_SETTINGS = {
     'MARGIN_PAGES_DISPLAYED': 2,
     'SHOW_FIRST_PAGE_WHEN_INVALID': True,
 }
-# SECRET_KEY = '123123'
 # override settings
 try:
     from .local import *
