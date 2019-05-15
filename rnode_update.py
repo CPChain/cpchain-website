348719c9c32baefab8b530f54bd90ba3edf761ce
zhangkai <zhangk@cpchain.io>
2019-05-15 03:10:56 -0700

    update one

--- rnode_update.py
+++ rnode_update.py
@@ -32,7 +32,7 @@ def save_rnode_proposer():
             # if proposer['Proposer'].endswith('000000'):
             #     proposer['Proposer'] = cf.cpc.getProposerByBlock(proposer['BlockNumber'])
             proposer_collection.update_one({}, {"$set": proposer}, upsert=True)
-            proposer_history_collection.update({"Term":proposer.get('Term')}, {"$set": proposer}, upsert=True)
+            proposer_history_collection.update_one({}, {"$set": proposer}, upsert=True)
 
         currentTerm = cf.cpc.getCurrentTerm
         if currentTerm:
