5a67e610f1f892fb324f7231f1d684cc7067e919
wujj <wujj@cpchain.io>
2019-06-17 19:50:54 +0800

    add lang

--- apps/wallet/urls.py
+++ apps/wallet/urls.py
@@ -3,5 +3,5 @@ from wallet.views import *
 
 urlpatterns = [
     path('news_detail/<pk>', community_detail),
-    path('news_list/', news_list),
+    path('news_list/<lang>', news_list),
 ]
--- apps/wallet/views.py
+++ apps/wallet/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from django.core import serializers
 import json
 
+
 # Create your views here.
 
 def community_detail(req, pk):
@@ -14,8 +15,12 @@ def community_detail(req, pk):
 # return msg:
 # "[{\"model\": \"wallet.walletnews\", \"pk\": 2, \"fields\": {\"category\": \"News\", \"title\": \"321\", \"banner\": \"\", \"update_time\": \"2019-06-17\", \"content\": \"<p>fff</p>\"}}]"
 
-def news_list(req):
-    news_list = WalletNews.objects.all()
-    data = serializers.serialize("json",news_list,
+def news_list(req, lang):
+    if lang == 'en':
+        news_list = WalletNews.objects.filter(category='News_en')
+    elif lang == 'cn':
+        news_list = WalletNews.objects.filter(category='News_cn')
+
+    data = serializers.serialize("json", news_list,
                                  fields=('pk', 'category', 'title', 'banner', 'update_time'))
     return JsonResponse(data, safe=False)
