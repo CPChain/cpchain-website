3990ccbc95cf5a6c0c6ae7eae341bdc321deae3d
wujj <wujj@cpchain.io>
2019-06-17 19:22:56 +0800

    list fix

--- apps/wallet/views.py
+++ apps/wallet/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 from django.core import serializers
-
+import json
 
 # Create your views here.
 
@@ -15,5 +15,7 @@ def community_detail(req, pk):
 # "[{\"model\": \"wallet.walletnews\", \"pk\": 2, \"fields\": {\"category\": \"News\", \"title\": \"321\", \"banner\": \"\", \"update_time\": \"2019-06-17\", \"content\": \"<p>fff</p>\"}}]"
 
 def news_list(req):
-    data = serializers.serialize("json", WalletNews.objects.all())
+    news_list = WalletNews.objects.all()
+    data = serializers.serialize("json",news_list,
+                                 fields=('pk', 'category', 'title', 'banner', 'update_time'))
     return JsonResponse(data, safe=False)
