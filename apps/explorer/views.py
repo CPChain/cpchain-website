a71ecf110a80a48dbf2a78e96fe09dacc26f0de0
wujj <wujj@cpchain.io>
2019-06-19 13:57:23 +0800

    fix

--- apps/explorer/views.py
+++ apps/explorer/views.py
@@ -506,7 +506,7 @@ def address(req, address):
     # address info
     txs = txs_collection.find({'$or': [{'from': address}, {'to': address}]}).sort('timestamp', DESCENDING)
     from_count = txs_collection.find({'from': address}).hint('from_1').count()
-    to_count = txs_collection.find({'from': address}).hint('to_1').count()
+    to_count = txs_collection.find({'to': address}).hint('to_1').count()
     both_count = txs_collection.count({'$and': [{'from': address}, {'to': address}]})
     txs_count = from_count + to_count - both_count
     try:
