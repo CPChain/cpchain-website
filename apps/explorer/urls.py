17e10ae1a51e5f91bf0a8e4389d177c8550bc6b2
zhangkai <zhangk@cpchain.io>
2019-06-27 22:46:45 -0700

    dev

--- apps/explorer/urls.py
+++ apps/explorer/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.explorer, name='explorer'),
-    path('dev/', views.explorerDev, name='explorer'),
+    path('dev/', views.explorerDev, name='explorerDev'),
     path('wshandler/', views.wshandler, name='wshandler'),
     path('blocks/', views.blocks, name='blocks'),
     path('block/<block_identifier>', views.block, name='block'),
