edc46e13923bf2ac66ed8b09d86db037642a5097
zgljl2012 <2693491512@qq.com>
2021-01-18 18:40:50 +0800

    VAC-821 RNode status API

--- apps/chain/urls.py
+++ apps/chain/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from django.db.models import base
 from rest_framework import routers
-from apps.chain.views import TxViewSet, RNodeRewardViewSet
+from apps.chain.views import RNodeStatusViewSet, TxViewSet, RNodeRewardViewSet
 
 router = routers.DefaultRouter()
 router.register('tx', TxViewSet, basename='tx')
-router.register('rnods-reward', RNodeRewardViewSet, basename='rnodes-reward')
+router.register('rnodes-reward', RNodeRewardViewSet, basename='rnodes-reward')
+router.register('rnode-status', RNodeStatusViewSet, basename='rnode-status')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
