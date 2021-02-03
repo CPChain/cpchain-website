536211ebd7dd6e9b0d8621ad2d1cd9d57d6c8cae
zgljl2012 <2693491512@qq.com>
2021-02-03 17:37:43 +0800

    VAC-822 news search api

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from apps.news.views import NewsView
+from apps.news.views import NewsView, MySearchView, NewsSearchView
 
 router = routers.DefaultRouter()
 router.register('news', NewsView, basename='news')
+router.register('search', NewsSearchView, basename='search')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    # url(r'search/', MySearchView(), name="news-search")
 ]
