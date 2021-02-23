f2dd59577ed388a20e52b99b17508f0957cf1a7c
zgljl2012 <2693491512@qq.com>
2021-02-23 14:32:36 +0800

    VAC-822 add is_handled for suggest

--- suggest/models.py
+++ suggest/models.py
@@ -4,5 +4,6 @@ class Suggest(models.Model):
     description = models.CharField(help_text="建议内容", blank=False, max_length=500)
     email = models.EmailField(help_text='用户 email')
     platform = models.CharField(help_text='提交平台：android/ios/browser 等', max_length=20)
+    handled = models.BooleanField(help_text='是否已处理', null=True, default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)    
